052bb810d64faf1c97b76ff570d74c64925e2aad
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-09 08:11:27 +0530

    font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
-        mentra: ['mentra', 'sans-serif'],
-        beckan: ['beckan', 'sans-serif'],
-        'gill-sans': ['gill sans', 'sans-serif'],
-        'gill-roy': ['gill roy', 'sans-serif'],
-        heavitas: ['heavitas', 'sans-serif'],
-        pearce: ['pearce', 'sans-serif'],
+        mentra: ["mentra", "sans-serif"],
+        beckan: ["beckan", "sans-serif"],
+        "gill-sans": ["gill sans", "sans-serif"],
+        "gill-roy": ["gill roy", "sans-serif"],
+        heavitas: ["heavitas", "sans-serif"],
+        pearce: ["pearce", "sans-serif"],
       },
     },
   },
